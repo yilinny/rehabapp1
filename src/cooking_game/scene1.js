e8b035c9e06957cdd4048eb706d6e08f88fef257
Yilinny <yilintan47@gmail.com>
2021-11-22 14:47:16 +0000

    step 0 and 1 done

--- src/cooking_game/scene1.js
+++ src/cooking_game/scene1.js
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect } from 'react'
 import './scene1.css'
 
@@ -8,32 +9,33 @@ function resizeContainer (){
 
  //have to be globally defined if not no work???
 
-export function Tap (){
+ function TapOn(){
+
     const [playState, setPlay] = useState('paused')
     const [seconds, setSeconds] = useState(0)
 
     useEffect(()=> {
         var second_interval;
     if (playState === 'running'){
-        second_interval = setInterval(()=> {setSeconds(seconds+ 1)},1000);
-        console.log(seconds)
+        if (second_interval){clearInterval(second_interval)}
+        second_interval = setTimeout(()=> {setSeconds(seconds+ 1)},1000);
+        
+        if (seconds >= 17){
+            setPlay('paused')
+            alert ('Oh no, you went past the line! Try again.')
+        }
+
     }
 
-    else if (playState === 'paused') {clearInterval(second_interval); 
+    else if (playState === 'paused') {clearInterval(second_interval)}; 
     
-    return function cleanup(){clearInterval(second_interval)
-    }
-    }}, [playState, seconds])
+    return function cleanup(){clearInterval(second_interval)}
+    }, [seconds, playState])
 
 
     return (
-        <div className = 'background'>
-        <div className= 'container'
-        style = {{
-                height: `${resizeContainer()}px`,
-                top: `${(window.innerHeight - resizeContainer())/2}px`,
-        }}> 
 
+        <div>
             <div className= 'water'
             style = {{
                     animation: `drip 1s linear infinite normal forwards ${playState}`,
@@ -45,20 +47,68 @@ export function Tap (){
             <div className= 'cup'> 
                <div className='waterlevel'
                 style = {{
-                    animation: `fade 20s ease-out 1 normal forwards ${playState}`
+                    animation: `fade 25s ease-out 1 normal forwards ${playState}`
                 }}></div>
             </div>
 
-
             <button className = 'btn' onClick = {() => {
                 playState === 'paused' ? setPlay('running') : setPlay('paused') 
-
             }}></button>
         </div>
-    </div>
+
+       
         
     )
 }
 
+function Instructions (props){
+    const instructions = [
+        'Fill the water',
+        'Bring water to pot',
+        'Add pasta'
+    ];
+
+    let required_instruction=[];
+
+    for (let i = 0; i < 3 ;i ++ ){
+        console.log('hi')
+        required_instruction.push(instructions[i])} 
+
+    console.log(required_instruction)
+    
+    return (
+        <div className= 'stepscontainer'>
+            {required_instruction.map(instruction => <p>{instruction}</p>)}
+            <button onClick= {props.onClick}> Let's start!</button>
+        </div>
+
+    ) //instructions and container needs serious css
+
+}
+
+export function KitchenOne (steps = 3) {
+
+    const [stepcount, setStep] = useState(0)
+    const renderscenes = [
+        function renderZero(){return(<Instructions steps = {steps} onClick={()=>{setStep(1)}} />)}, 
+        function renderOne (){return(<TapOn/>)}
+    ] //an array of functions, each rendering the desired scene. stepcount as index for the scene. Parent container of the scene is the div w background image 
+    
+
+
+    return (
+        <div className = 'background'>
+        <div className= 'container'
+        style = {{
+                height: `${resizeContainer()}px`,
+                top: `${(window.innerHeight - resizeContainer())/2}px`,
+        }}> 
+        {renderscenes[stepcount]()} 
+         </div>
+    </div>)
+    
+}
+
+
 //2 times water is water animation 
-export default Tap
+export default KitchenOne
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import { GameMenu } from './gamemenu/gamemenu';
 import { SquareSettings } from './square/squaresettings';
 import {PuzzleSettings} from './puzzle/puzzlesettings';
-import {Tap} from './cooking_game/scene1' 
+import {KitchenOne} from './cooking_game/scene1' 
 
 const Routing = () => {
   return(
@@ -16,7 +16,7 @@ const Routing = () => {
         <Route path="/" element={ <GameMenu /> } />
         <Route path="/square" element={ <SquareSettings /> } />
         <Route path="/puzzle" element={ <PuzzleSettings /> } />
-        <Route path= "/cooking" element = {<Tap/> } />
+        <Route path= "/cooking" element = {<KitchenOne/> } />
       </Routes> 
     </Router >
   );
