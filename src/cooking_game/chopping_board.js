87d6cd1fc4d8fed830523a0767085b9826eeea1b
Depressionn <tantiongguan123@gmail.com>
2021-12-18 23:56:47 +0800

    chopping board draft

--- src/cooking_game/chopping_board.js
+++ src/cooking_game/chopping_board.js
@@ -6,8 +6,23 @@ function resizeContainer (){
     return (1080/1920 * window.innerWidth)
 }
 
-const ChoppingBoard = ({recipeNo = 2, stepNo = 2}) =>{
+const ChoppingBoard = ({recipeNo = 2, stepNo = 2, difficulty = 1}) =>{
     let task = recipes[recipeNo].step[stepNo]
+    const [width, setWidth] = useState(50)
+    const [chopno, setChopno] = useState(0)
+    const correctchop = task.adapt.chop_no[difficulty]
+
+    console.log(`correctchop is ${correctchop}`)
+
+    const Chopping = () =>{
+        //total width is 50
+        //each chop should reduce width by correct chopno/50
+        setWidth(50 - chopno*correctchop/50)
+        console.log(`new width is ${50 - chopno*correctchop/50}`)
+        setChopno(chopno + 1)
+
+        if (chopno === correctchop){console.log('yay')}
+    }
 
     return (
         <div className = 'background'>
@@ -18,28 +33,31 @@ const ChoppingBoard = ({recipeNo = 2, stepNo = 2}) =>{
                 top: `${(window.innerHeight - resizeContainer())/2}px`,
         }}> 
 
+        <div className='timebar'></div>
+
+        <div className='timebar' style={{
+            backgroundColor: 'green',
+            width: `${50-width}%`
+        }}></div>
+
         <div className='ingredients' key='cut' style={{
              backgroundImage: `url(/fridge_pics/ingredientc-${task.ing[0]}.png)`,
              left: '25%',
              top: '28.5%',
              width: '50%',
              height: '50%',
-        }}></div>
+        }}></div> 
 
-        <div className='ingredients' style = {{
+        <div className='ingredients' onClick= {Chopping} style = {{
             backgroundImage: `url(/fridge_pics/ingredienth-${task.ing[0]}.png)`,
             left: '25%',
             top: '30%',
-            width: '50%',
+            width: `${width}%`,
             height: '50%',
-            backgroundSize: 'cover',
-            animation: 'chopchop 5s linear 0s 1 normal forwards running'
-        }}> </div> 
+            backgroundSize: 'cover'
+        }} > </div> 
 
         </div></div>
-
-
-
     )
 }
 
