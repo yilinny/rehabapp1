9cb8e6415219d4a1457728331b76cfa9360d5777
Yilinny <yilintan47@gmail.com>
2022-01-17 14:53:44 +0000

    game menu rough draft done

--- src/gamemenu/gamemenu.js
+++ src/gamemenu/gamemenu.js
@@ -10,26 +10,30 @@ export class GameMenu extends React.Component {
                     id: 'jigsaw-puzzle',
                     name: 'Jigsaw Puzzle',
                     link: '/puzzle',
-                    description: 'Complete Jigsaw Puzzles!'
+                    description: 'Jigsaw puzzles from 4-84 pieces to complete. Option to turn own photos into puzzle as well!',
+                    tags: 'visual processing, sustained attention, gross motor (drag and drop)'
                 },
 
                 {
                     id: 'square-game',
                     name: 'Tap the Square',
                     link: '/square',
-                    description: 'Simple tap the square game'
+                    description: 'Tap the square! High scores and level ups provide motivation to keep at repetition needed.',
+                    tags: 'gross motor/repetition, information processing, visual assessment, accuracy training'
                 },
                 {
                     id: 'cooking-game',
                     name: 'Cooking game',
                     link: '/cooking',
-                    description: 'Be your own coooking mama!'
+                    description: 'Choose from a variety of recipes, grade according to cognitive/physical skills.',
+                    tags: 'occupation based, attention, working memory, information processing, gross motor'
                 },
                 {
                     id: 'dino-game',
                     name: 'Dino',
                     link: '/dino',
-                    description: 'Dinosaur jump!'
+                    description: 'The classic chrome dinosaur game! Pay attention and help dino avoid the obstacles',
+                    tags: 'sustained attention, information processing'
                 }
             ]
         };
@@ -37,20 +41,21 @@ export class GameMenu extends React.Component {
 
     render() {
         return (
-            <div className="" style={{ height: '100%', width: '100%', backgroundColor: 'black' }}>
+            <div className="" style={{ height: '100%', width: '100%', backgroundColor: '#006D77' }}>
                 {
                     this.state.games.map((game, index) => {
                         return (
-                            <Card style={{ top: `${(Math.floor(index * 0.5) * 30 + 20)}%`, left: `${(index % 2) * 40 + 20}%`, position: 'absolute' }}>
+                            <Card style={{ backgroundColor: '#83C5BE', top: `${(Math.floor(index * 0.5) * 40 + 15)}%`, left: `${(index % 2) * 38 + 18}%`, position: 'absolute', width: '30%' , height: '35%'}}>
                                 <Card.Content>
-                                    <Image src='game_menu_pics/bg-2.png' wrapped ui={true}></Image>
+                                    <Image src={`game_menu_pics/bg-${index}.png`} wrapped ui={true}></Image>
                                     <Card.Header>{game.name}</Card.Header>
                                     <Card.Meta>
-                                        <span className='date'>{game.name}</span>
+                                        <span className='date'>{game.tags}</span>
                                     </Card.Meta>
                                     <Card.Description>
                                         {game.description}
                                     </Card.Description>
+                                    <br/>
                                     <Button href={game.link}> Play now!</Button>
                                 </Card.Content>
                             </Card>
